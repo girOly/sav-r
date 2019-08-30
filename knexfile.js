f7543a6db37cb8f203318fda5856f8442a9fffd2
girOly <girardob@gmail.com>
2019-08-30 15:19:39 -0400

    Node modules and initial setup for back end

---
+++ knexfile.js
@@ -0,0 +1,58 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./db/knex_migrations"
+    },
+    seeds: {
+      directory: "./db/knex_seeds"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./db/knex_migrations"
+    },
+    seeds: {
+      directory: "./db/knex_seeds"
+    }
+  }
+};
