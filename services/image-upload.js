70791caeb09e1d7bf62a18b575ab3683f55b56d7
robinwebber <robinewebber@gmail.com>
2019-08-31 14:44:54 +0000

    AWS keys added to .env

--- services/image-upload.js
+++ services/image-upload.js
@@ -1,14 +1,16 @@
-var aws = require("aws-sdk");
-var express = require("express");
-var multer = require("multer");
-var multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+require("dotenv").config();
 
-var app = express();
-var s3 = new aws.S3({
-  /* ... */
+aws.congif.update({
+  secretAccessKey: process.env.AWS_SECRET_ACCESS,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: "process.env."
 });
+const s3 = new aws.S3();
 
-var upload = multer({
+const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: "some-bucket",
