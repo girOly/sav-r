fca47e14ad5c1a767df21fa11f7e627a96db268d
Robin Webber <rwebber@Robins-MacBook-Pro.local>
2019-09-01 22:49:38 -0400

    textParser now testable without making api call

---
+++ parserTesting/parserTest.js
@@ -0,0 +1,18 @@
+const textParser = require("../services/text-parser");
+
+const hM = require("./test_data/hM");
+const iga = require("./test_data/iga");
+const ikea = require("./test_data/ikea");
+const loblaws = require("./test_data/loblaws");
+const metro = require("./test_data/metro");
+const walmart = require("./test_data/walmart");
+const { timHortons, timHortons2 } = require("./test_data/timHortons");
+
+textParser(hM);
+textParser(iga);
+textParser(ikea);
+textParser(loblaws);
+textParser(metro);
+textParser(walmart);
+textParser(timHortons);
+textParser(timHortons2);
