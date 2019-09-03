9dfc2178a479e7f345aa93ab65067df81635f206
Robin Webber <rwebber@Robins-MacBook-Pro.local>
2019-09-03 17:51:39 -0400

    starting uploader component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
 import React from "react";
 import "./App.css";
+import Menu from "../src/components/menu";
 import useApplicationData from "./hooks/useApplicationData";
 function App() {
-  const { state, dispatch } = useApplicationData();
-
-  const userList = state.users.map(user => (
-    <li key={user.id}>
-      {user.first_name} {user.last_name} {user.email}
-    </li>
-  ));
-
-  return (
-    <div className="App">
-      <h1>List of users</h1>
-
-      <ul>{userList}</ul>
-    </div>
-  );
+  return <Menu />;
 }
 
 export default App;
