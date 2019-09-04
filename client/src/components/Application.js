69bcb33cc096d6c914395cc7be2f6fea67c312e1
Robin Webber <robinewebber@gmail.com>
2019-09-04 13:24:48 -0400

    menu.js changed to Main-menu.js

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Router, Route, Switch } from "react-router";
 import { useVisualMode } from "../hooks/useVisualMode";
 import Uploader from "./Uploader";
-import Menu from "./Menu";
+import Menu from "./Main-menu";
 
 const MENU = "MENU";
 const UPLOAD = "UPLOAD";
 
 export default function Application(props) {
-  const { mode, transition, back } = useVisualMode(MENU);
   return <Menu />;
 }
