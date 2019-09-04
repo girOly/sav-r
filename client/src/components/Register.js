77e5c63fcd335546472fb89f75401e4976322004
Robin Webber <robinewebber@gmail.com>
2019-09-04 18:58:00 -0400

    register inputs controlled

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,19 +2,53 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Register(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
+  const makeUser = () => {
+    console.log("name: ", name);
+    console.log("email: ", email);
+    console.log("password: ", password);
+    return axios.post("/api/users/register");
+  };
+
   return (
     <div>
       <h1>REGISTER</h1>
-      <form>
+      <form onSubmit={event => event.preventDefault()}>
         name
-        <input type="text" name="name" />
+        <input
+          onChange={event => setName(event.target.value)}
+          type="text"
+          name="name"
+        />
         email
-        <input type="text" name="email" />
+        <input
+          onChange={event => setEmail(event.target.value)}
+          type="text"
+          name="name"
+        />
         password
-        <input type="text" name="password" />
-        <button type="submit">Register</button>
+        <input
+          onChange={event => setPassword(event.target.value)}
+          type="text"
+          name="name"
+        />
+        <button onClick={makeUser}>Register</button>
       </form>
       <button onClick={props.toLogin}>Take me to login</button>
     </div>
   );
+}
+
+{
+  /* <input
+  onChange={event => setEmail(event.target.value)}
+  type="text"
+  name="email"
+/>; */
 }
