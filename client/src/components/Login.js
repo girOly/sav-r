5436f99d7389d36512b2c1f1377c164af1996666
Robin Webber <robinewebber@gmail.com>
2019-09-04 15:59:10 -0400

    small change

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Login(props) {
+  [email, setEmail] = useState("");
+
   return (
     <div>
       <h1>LOGIN</h1>
