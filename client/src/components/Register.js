ad5f1a7bfb5f932486cd092688705660aa4ffd9e
Robin Webber <robinewebber@gmail.com>
2019-09-04 18:30:45 -0400

    branching

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Register(props) {
