ec5bfbb5bc76c23242cd1feea990303e64c4515d
girOly <girardob@gmail.com>
2019-09-04 17:30:01 +0000

    budget seed

--- db/knex_seeds/c_addbudgets.js
+++ db/knex_seeds/c_addbudgets.js
@@ -8,12 +8,14 @@ exports.seed = function(knex) {
       return knex("budgets").insert([
         {
           income: 35000000,
+          start_date: Date.now(),
           end_date: null,
           userID: 1,
           savingID: 1
         },
         {
           income: 35000000,
+          start_date: Date.now(),
           end_date: null,
           userID: 2,
           savingID: 2
