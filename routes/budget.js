6f38f39571240e6d3a3cb435182d46253add967f
girOly <girardob@gmail.com>
2019-09-04 02:22:39 -0400

    budget post route -tested

--- routes/budget.js
+++ routes/budget.js
@@ -11,11 +11,28 @@ module.exports = function(knex) {
   router.get("/jao", function(req, res) {
     knex
       .select("*")
-      .from("budget")
+      .from("budgets")
       .then(result => {
         res.json(result);
       })
       .catch(error => console.log(error));
+  });
+
+  router.post("/", function(req, res) {
+    knex("budgets")
+      .insert(req.body, ["id"])
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
+    // req.body from front end = {
+    // {
+    // 	"income":   35000,
+    //  "start_date": "2019-08-30 22:28:36.438072+00",
+    //  "end_date": null,
+    //  "user_id": 1,
+    // 	"saving_id": 1
+    //      }
   });
 
   return router;
