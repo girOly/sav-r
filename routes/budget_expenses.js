923b2c828afda1b79df36d56e1af87b602c945b9
girOly <girardob@gmail.com>
2019-09-04 02:22:28 -0400

    routes savings, budget_expenses -tested

--- routes/budget_expenses.js
+++ routes/budget_expenses.js
@@ -18,5 +18,20 @@ module.exports = function(knex) {
       .catch(error => console.log(error));
   });
 
+  router.post("/", function(req, res) {
+    knex("budget_expenses")
+      .insert(req.body, ["id"])
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
+    // req.body from front end = {
+    // {
+    //     	"total_cents":   1000,
+    //      "comments": "kyle owes me half",
+    //      "budget_id": 2,
+    //      "category_id": 1 }
+  });
+
   return router;
 };
--- routes/savings.js
+++ routes/savings.js
@@ -17,6 +17,20 @@ module.exports = function(knex) {
       })
       .catch(error => console.log(error));
   });
+  router.post("/", function(req, res) {
+    knex("savings")
+      .insert(req.body, ["id"])
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
+    // req.body from front end = {
+    // {
+    //     	"name":   "new car",
+    //      "target": 400000,
+    //      "progress": 200000,
+    //      "user_id": 1 }
+  });
 
   return router;
 };
