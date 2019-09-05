043821268bae25746d78bb916be1dd8c3e0ff2f7
Robin Webber <robinewebber@gmail.com>
2019-09-05 11:03:03 -0400

    passwords hashed in db

--- routes/users.js
+++ routes/users.js
@@ -50,12 +50,24 @@ module.exports = function(knex) {
   });
 
   router.post("/register", function(req, res) {
-    knex("users")
-      .insert(req.body, ["id"])
-      .then(result => {
-        res.json(result);
-      })
-      .catch(error => console.log(error));
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    console.log("route name:", name);
+    console.log("route password:", password);
+    console.log("route email:", email);
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+
+      knex("users")
+        .insert({ name: name, password: hash, email: email }, ["id"])
+        .then(result => {
+          res.json(result);
+        })
+        .catch(error => console.log(error));
+    });
     // req.body from front end = {
     // {
     //         "name":   "oaj",
@@ -65,6 +77,7 @@ module.exports = function(knex) {
 
   router.post("/login", function(req, res) {
     // authenticateUser(req, res, req.body.email);
+
     knex("users")
       .select("*")
       .from("users")
