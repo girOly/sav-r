10451274b1a6a617260a44eccda4bb0f6dce59d0
girOly <girardob@gmail.com>
2019-09-05 21:46:14 +0000

    testing

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -16,14 +16,14 @@ export default function Uploader(props) {
     axios
       .post(
         "/api/image-upload",
-        { test: "123" },
-        {
-          headers: {
-            accept: "application/json",
-            "Accept-Language": "en-US,en;q=0.8",
-            "Content-Type": "multipart/form-data;"
-          }
-        }
+        { key: "string" }
+        // {
+        //   headers: {
+        //     accept: "application/json",
+        //     "Accept-Language": "en-US,en;q=0.8",
+        //     "Content-Type": "multipart/form-data;"
+        //   }
+        // }
       )
       .then(response => {
         console.log(file);
