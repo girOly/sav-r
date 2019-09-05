2a578cabc388a61cd6b6753b7c18e5af6d28e8ab
girOly <girardob@gmail.com>
2019-09-05 21:46:11 +0000

    testing

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import FormData from "form-data";
 
 export default function Uploader(props) {
-  const [file, setFile] = useState({});
+  const [file, setFile] = useState("");
   console.log(file);
 
   const uploadFile = () => {
+    let formdata = new FormData();
     console.log("file: ", file);
     // console.log("email: ", email);
     // console.log("password: ", password);
-
-    return axios
-      .post("http://localhost:3005/api/image-upload", {
-        file
-      })
+    formdata.append("image", file);
+    axios
+      .post(
+        "/api/image-upload",
+        { test: "123" },
+        {
+          headers: {
+            accept: "application/json",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Content-Type": "multipart/form-data;"
+          }
+        }
+      )
       .then(response => {
+        console.log(file);
         console.log("axios-response", response);
       })
       .catch(err => {
