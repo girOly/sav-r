8d2686921ba09f16e562442c541d2a07682b1ec2
Robin Webber <robinewebber@gmail.com>
2019-09-08 19:11:50 -0400

    create budget flexed

--- client/src/components/Create-budget.js
+++ client/src/components/Create-budget.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./Create-budget.css";
 
 export default function CreateBudget(props) {
   const [income, setIncome] = useState(0);
   const [budgetName, setBudgetName] = useState("");
-
-  console.log(budgetName);
-
-  console.log(income);
 
   const budgetCreate = () => {
     return axios
@@ -25,26 +22,44 @@ export default function CreateBudget(props) {
   };
 
   return (
-    <div>
-      <Link to="/">MENU</Link>
-      <h1>CREATE BUDGET</h1>
-      <form onSubmit={event => event.preventDefault()}>
-        Budget name
-        <input
-          onChange={event => setBudgetName(event.target.value)}
-          type="text"
-          name="budget_name"
-        />
-        Income
-        <input
-          onChange={event => setIncome(event.target.value)}
-          type="number"
-          name="income"
-        />
-        <button onClick={budgetCreate} type="submit">
-          Submit
-        </button>
-      </form>
+    <div className="newBudgetBigBox">
+      <div className="newBudgetMain">
+        <div className="newBudgetLogoMenu">
+          <div className="newBudgetMenuBox">
+            <Link to="/">MENU</Link>
+          </div>
+          <h3 className="newBudgetSavrLogo">Sav-r</h3>
+        </div>
+        <div className="newBudgetName">
+          <h2> Create budget</h2>
+        </div>
+        <div className="newBudgetForm">
+          <div className="newBudgetFormCenter">
+            <form
+              className="newBudgetActualForm"
+              onSubmit={event => event.preventDefault()}
+            >
+              <div className="newBudgetFormName">
+                Budget name
+                <input
+                  onChange={event => setBudgetName(event.target.value)}
+                  type="text"
+                  name="budget_name"
+                />
+                How much can you spend?
+                <input
+                  onChange={event => setIncome(event.target.value)}
+                  type="number"
+                  name="income"
+                />
+                <button onClick={budgetCreate} type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
