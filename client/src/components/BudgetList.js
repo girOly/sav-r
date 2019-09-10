674012bdf3f105639eb03105f9d355e9b69fdb5b
Robin Webber <robinewebber@gmail.com>
2019-09-09 23:59:55 -0400

    select budget styled

--- client/src/components/BudgetList.js
+++ client/src/components/BudgetList.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import BudgetListItem from "./BudgetListItem";
+import "./BudgetList.css";
 
 export default function BudgetList(props) {
   const { budgets } = props;
 
   const budgetsMap = budgets.map(budget => (
-    <BudgetListItem
-      key={budget.id}
-      budget={budget}
-      chooseBudget={props.chooseBudget}
-    />
+    <div className="budgetListBox">
+      <BudgetListItem
+        key={budget.id}
+        budget={budget}
+        chooseBudget={props.chooseBudget}
+      />
+    </div>
   ));
 
   return <ul>{budgetsMap}</ul>;
